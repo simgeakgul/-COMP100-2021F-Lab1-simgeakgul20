23a50c4b44f56f2b49792d0d99bc605f1e006152
simgeakgul <91738792+simgeakgul@users.noreply.github.com>
2021-10-08 19:15:08 +0000

    lab1 quiz

--- main.py
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir profiles2021
-touch profiles2021/helin.txt
-ls
-
--- main.sh
+++ main.sh
@@ -9,12 +9,15 @@ rm ids.txt
 #### DO NOT EDIT ANYTHING ABOVE THIS LINE #####
 #### ==================================== #####
 
-
 # Command for Q1
+touch quiz.sh
+echo #!/bin/sh >> quiz.sh
 
 # Command for Q2
+chmod a+rwx quiz.sh
 
 # Command for Q3
+cat profiles2021/simge.txt > quiz.sh
 
 # Command for Q4
-
+cp profiles2020/* profiles2021
